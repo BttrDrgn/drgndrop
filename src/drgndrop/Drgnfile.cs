a173840b877f002bfc57a8d31990a16697f971fa
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2024-01-02 02:46:36 -0600

    refactor(drgndrop): moved DrgnfileInfo

--- src/drgndrop/Drgnfile.cs
+++ src/drgndrop/Drgnfile.cs
@@ -4,16 +4,6 @@ using System.Text.Json;
 
 namespace drgndrop
 {
-    public class DrgnfileInfo
-    {
-        public string ID { get; set; }
-        public string Name { get; set; }
-        public long Creation { get; set; }
-        public long Size { get; set; }
-        public string PasswordHash { get; set; }
-        public bool IsEncrypted => !String.IsNullOrEmpty(PasswordHash);
-    }
-
     public class Drgnfile
     {
         public DrgnfileInfo Info;
---
+++ src/drgndrop/Models/DrgnfileInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace drgndrop
+{
+    public class DrgnfileInfo
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public long Creation { get; set; }
+        public long Size { get; set; }
+        public string PasswordHash { get; set; }
+        public bool IsEncrypted => !String.IsNullOrEmpty(PasswordHash);
+    }
+}
