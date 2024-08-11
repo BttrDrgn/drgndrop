2442b11f86353d945151b32c2b5459153e56b728
BttrDrgn <coreycannon321@gmail.com>
2024-08-11 08:14:00 -0500

    fix(drgndrop): potential security risk reduction

--- src/drgndrop/Database.cs
+++ src/drgndrop/Database.cs
@@ -53,10 +53,21 @@ namespace drgndrop
         }
 
         //USERS
-        public static User? GetUserByName(string name)
+        public static User? GetUserByName(string name, bool admin = false)
         {
             List<User> tempUsers = Users.Query().Where(x => x.Name == name.ToLower()).ToList();
-            if (tempUsers.Count == 1) return tempUsers[0];
+            if (tempUsers.Count == 1)
+            {
+                //Im not sure if blazor leaks these values into the client with server rendering but theres no reason to risk it i guess
+                User retn = tempUsers[0];
+                if (!admin)
+                {
+                    retn.SessionToken = "";
+                    retn.GUID = "";
+                    retn.Password = "";
+                }
+                return retn;
+            }
             return null;
         }
 
@@ -73,10 +84,21 @@ namespace drgndrop
             return null;
         }
 
-        public static User? GetUserByGUID(string guid)
+        public static User? GetUserByGUID(string guid, bool admin = false)
         {
             List<User> tempUsers = Users.Query().Where(x => x.GUID == guid.ToLower()).ToList();
-            if (tempUsers.Count == 1) return tempUsers[0];
+            if (tempUsers.Count == 1)
+            {
+                //Im not sure if blazor leaks these values into the client with server rendering but theres no reason to risk it i guess
+                User retn = tempUsers[0];
+                if (!admin)
+                {
+                    retn.SessionToken = "";
+                    retn.GUID = "";
+                    retn.Password = "";
+                }
+                return retn;
+            }
             return null;
         }
 
